882270d4e048003b7cfada26bdee615c8aed5f3c
Vinod <vinodshamraaj@gmail.com>
2025-02-03 23:54:58 +0800

    feat: Added CORS config for localhost

---
+++ src/main/java/com/assessment/maybank/springboot_api/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.assessment.maybank.springboot_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowCredentials(true); // Allow credentials (cookies, authorization headers)
+        config.setAllowedOrigins(List.of("http://localhost:3000")); // Set allowed origins
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allowed HTTP methods
+        config.setAllowedHeaders(List.of("*")); // Allow all headers
+        
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
